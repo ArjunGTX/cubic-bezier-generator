e648db0972de46c5f94c098888e4f55eebe37a70
Arjun <arjunvc.official@gmail.com>
2025-01-01 16:43:07 +0530

    FEAT: UI, logic and implementation for generator  (#1)
    * chore: setup base Ui template
    
    * chore: added event handler for copy button
    
    * chore: added logic for generating curve based on point coordinates
    
    * chore: removed all redundant styles
    
    * chore: convert the implementation into OOP
    
    * chore: added meta description
    
    * chore: added readme file
    
    ---------
    
    Co-authored-by: ArjunGTX <arjun@usefindr.com>

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
+import { CurveGenerator } from "./curve-generator";
 import "./style.css";
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
+const copyButton = document.getElementById(
+  "copy-btn",
+) as HTMLButtonElement | null;
+const output = document.getElementById("output") as HTMLElement | null;
+const box = document.getElementById("box") as HTMLDivElement | null;
+
+if (!canvas) {
+  throw new Error("Canvas element not found!");
+}
+
+const curve = new CurveGenerator(canvas);
+
+curve.onCurveChange((curveCords) => {
+  const curveString = curve.generateCurveString(curveCords);
+  if (output) {
+    output.textContent = `animation-timing-function: ${curveString}`;
+  }
+  if (box) {
+    box.style.animationTimingFunction = curveString;
+  }
+});
+
+// Enable copying the curve value to the clipboard
+copyButton?.addEventListener("click", function () {
+  navigator.clipboard.writeText(
+    `animation-timing-function: ${curve.getCurrentCurveString()}`,
+  );
+  this.textContent = "Copied!";
+  // Reset the button text after 1 second
+  setTimeout(() => {
+    this.textContent = "Copy";
+  }, 1000);
+});
