a799e37dc98998c82d028b721892fb1f46d0bb23
ArjunGTX <arjun@usefindr.com>
2025-01-01 11:31:06 +0530

    chore: added event handler for copy button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+
+let curveValue = "";
+
+const copyButton = document.getElementById("copy-btn");
+copyButton?.addEventListener("click", function () {
+  navigator.clipboard.writeText(curveValue);
+  this.textContent = "Copied!";
+  setTimeout(() => {
+    this.textContent = "Copy";
+  }, 1000);
+});
